a7a91beecf28b9a1c713216389469bd91e5a15c0
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-06-07 15:33:11 +0700

    Register settings

--- plugin.gd
+++ plugin.gd
@@ -2,11 +2,23 @@
 extends EditorPlugin
 
 
+var default_settings: Dictionary[String, Variant] = {
+	"application/game_jolt/game_id": 0,
+	"application/game_jolt/private_key": "",
+}
+
 func _enter_tree() -> void:
-	# Initialization of the plugin goes here.
-	pass
+	for key in default_settings.keys():
+		if !ProjectSettings.has_setting(key):
+			ProjectSettings.set_setting(key, default_settings[key])
+		ProjectSettings.set_initial_value(key, default_settings[key])
+		ProjectSettings.set_as_basic(key, true)
+		ProjectSettings.add_property_info({
+			"name": key,
+			"type": typeof(default_settings[key]),
+		})
 
 
 func _exit_tree() -> void:
-	# Clean-up of the plugin goes here.
-	pass
+	for key in default_settings.keys():
+		ProjectSettings.set_setting(key, null)
